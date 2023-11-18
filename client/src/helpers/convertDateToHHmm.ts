c1655edc82902ad45069f8973c7b9671da2c9973
Ivan Kotov <iamivankotov@gmail.com>
2023-11-18 12:36:06 +0100

    refactoring

--- client/src/helpers/convertDateToHHmm.ts
+++ client/src/helpers/convertDateToHHmm.ts
@@ -1,6 +1,8 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 export const convertDateToHHmm = (d: Date) => {
-	if (d) return dayjs(d).format('HH:mm')
+	if (dayjs(d).isValid()) {
+		return dayjs(d).format('HH:mm');
+	}
 	return '??';
 }

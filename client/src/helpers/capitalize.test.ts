0bd3c40a4cd665ade1d38eb934bc605640ae82c6
Ivan Kotov <iamivankotov@gmail.com>
2023-11-18 12:34:30 +0100

    unit test

---
+++ client/src/helpers/capitalize.test.ts
@@ -0,0 +1,15 @@
+import { capitalize } from './capitalize';
+
+describe('capitalize', () => {
+  it('capitalizes the first letter of a string', () => {
+    expect(capitalize('test')).toBe('Test');
+  });
+
+  it('returns an empty string when given an empty string', () => {
+    expect(capitalize('')).toBe('');
+  });
+
+  it('doesnt change a string that had already capitalized', () => {
+    expect(capitalize('Test')).toBe('Test');
+  });
+});

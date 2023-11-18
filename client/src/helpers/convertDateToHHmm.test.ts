0bf9957968e84c34951cd2391d027989a9a24691
Ivan Kotov <iamivankotov@gmail.com>
2023-11-18 12:38:08 +0100

    unit test

---
+++ client/src/helpers/convertDateToHHmm.test.ts
@@ -0,0 +1,14 @@
+import dayjs from 'dayjs';
+import { convertDateToHHmm } from './convertDateToHHmm';
+
+describe('convertDateToHHmm', () => {
+  it('formats a valid date to HH:mm', () => {
+    const date = new Date('2023-01-01T15:30:00');
+    expect(convertDateToHHmm(date)).toBe(dayjs(date).format('HH:mm'));
+  });
+
+  it('returns "??" for an invalid date', () => {
+    const invalidDate = new Date('Invalid Date');
+    expect(convertDateToHHmm(invalidDate)).toBe('??');
+  });
+});

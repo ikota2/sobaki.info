ce853bf2936ac52288590162876fec29f8dd0a9d
Ivan Kotov <iamivankotov@gmail.com>
2023-11-18 15:01:47 +0100

    layout

--- client/src/features/Finder/Finder.tsx
+++ client/src/features/Finder/Finder.tsx
@@ -1,9 +1,9 @@
-import React, {FC, useState} from "react"
+import React, {Component, FC, useState} from "react";
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 
-import classes from './Finder.module.scss';
 import {AllStations} from "../../types";
-
+import classes from './Finder.module.scss';
+// ⇄
 export interface Values {
   from: string;
   to: string;
@@ -17,11 +17,8 @@ interface Props {
 const Finder: FC<Props> = ({setParams, allStations}) => {
   const [swap, setSwap] = useState(false);
 
- // console.log('finder');
-  const titles: string[] | undefined = allStations?.map(({stations}) => stations.map(({title}) => title)).flat();
-
   return (
-    <>
+    <div className={classes.container}>
       <Formik
         initialValues={{
           from: '',
@@ -44,23 +41,39 @@ const Finder: FC<Props> = ({setParams, allStations}) => {
         }}
       >
         <Form>
-          <Field id={!swap ? 'from' : 'to'} name={!swap ? 'from' : 'to'} placeholder="Откуда" list="megalist" />
-          {/*<datalist id="megalist">*/}
-          {/*  {titles?.map((title) => <option value={title} key={title} />)}*/}
-          {/*</datalist>*/}
+          <Field
+            id={!swap ? 'from' : 'to'}
+            name={!swap ? 'from' : 'to'}
+            placeholder="Откуда" list="megalist"
+            style={{height: '30px'}}
+          />
           <button
             type="button"
             onClick={() => setSwap(!swap)}
-            className={classes.swap}
             data-testid="swapper"
-          >⇄</button>
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width='16px' height='16px'>
+              <g id="_82-Arrow_Exchange" data-name="82-Arrow Exchange">
+                <path d="M23.29,15.29l1.41,1.41,7-7a1,1,0,0,0,0-1.41l-7-7L23.29,2.71,28.59,8H0v2H28.59Z"/>
+                <path d="M8.71,16.71,7.29,15.29l-7,7a1,1,0,0,0,0,1.41l7,7,1.41-1.41L3.41,24H32V22H3.41Z"/>
+              </g>
+            </svg>
+          </button>
 
-          <Field id={swap ? 'from' : 'to'} name={swap ? 'from' : 'to'} placeholder="Куда" list="megalist"/>
+          <Field id={swap ? 'from' : 'to'} name={swap ? 'from' : 'to'} placeholder="Куда" list="megalist"
+                 style={{height: '30px', borderRadius: '5px'}}
+          />
 
-          <button type="submit" data-testid="submit">Submit</button>
+          <button type="submit" data-testid="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width='20px' height='25px'>
+              <g id="_49-Search" data-name="49-Search">
+                <path d="M21.15,19.74a12,12,0,1,0-1.41,1.41L30.29,31.71l1.41-1.41ZM12,22A10,10,0,1,1,22,12,10,10,0,0,1,12,22Z"/>
+              </g>
+            </svg>
+          </button>
         </Form>
       </Formik>
-    </>
+    </div>
   )
 }
 
